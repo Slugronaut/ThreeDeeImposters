e1c0eca4f676385ad3cb26ffe2bc514229be678e
Slugronaut <jcsluggy@gmail.com>
2023-06-28 23:48:06 -0400

    Simplified system and improved performance
    -There is no longer a centralized system that needs to sync rendering to a camera.
    -The need for a camera in the scene has also been removed.
    -Imposters now pre-render the frame after they start and do not have to queue a command that is rendered one-at-a-time each frame.

--- Runtime/ThreeDeeSpriteImposter.cs
+++ Runtime/ThreeDeeSpriteImposter.cs
@@ -1,6 +1,7 @@
 using Sirenix.OdinInspector;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace ThreeDee
 {
@@ -10,31 +11,6 @@ namespace ThreeDee
     /// </summary>
     public class ThreeDeeSpriteImposter : MonoBehaviour
     {
-        /*
-        #region Shared
-        //Eventually these shared values will be pushed into an SO that will be referenced by this class instead.
-        [Tooltip("The RenderTexture to which this imposter will be rendered.")]
-        public RenderTexture TargetPrefab;
-
-        [Tooltip("The material that will be duplicated and applied to the billboard.")]
-        public Material ImposterBillboardMatPrefab;
-
-        [Tooltip("")]
-        public Vector3 Offset = Vector3.zero;
-
-        [Tooltip("")]
-        public float Scale = 1;
-
-        [Tooltip("")]
-        public Vector3 Rot;
-
-        [Tooltip("If true, this will attempt to bake a skinned mesh renderer on the sprite model. Otherwise it will search for and use a regualr mesh renderer.")]
-        public bool SkinnedMesh = true;
-
-        [Tooltip("The animation pose to use for the imposter.")]
-        public string DefaultAnim = "Idle Stand 01";
-        #endregion
-        */
 
         [Tooltip("A reference to an ImposterAsset containing common state data that may be shared by many different imposters.")]
         public ImposterAsset SharedData;
@@ -43,7 +19,6 @@ namespace ThreeDee
         public MeshRenderer ImposterBillboardRenderer;
 
         RenderTexture BillboardTarget;
-
 
 
         private IEnumerator Start()
@@ -83,7 +58,8 @@ namespace ThreeDee
             var tdsRend = GetComponentInChildren<IThreeDeeSpriteRenderer>(true);
             var skinRend = tdsRend.ModelTrans.GetComponentInChildren<SkinnedMeshRenderer>(true);
 
-            //setup the animator state and pump it. that way when processing the issued command we can bake the correct anim
+            //make sure our 3D model is active and then setup the animator state and pump it.
+            //that way when processing the issued command we can bake the correct anim
             var anim = tdsRend.ModelTrans.GetComponentInChildren<Animator>(true);
             var info = anim.GetCurrentAnimatorStateInfo(0);
             bool oldState = tdsRend.ModelTrans.gameObject.activeSelf;
@@ -93,18 +69,98 @@ namespace ThreeDee
             if (!tdsRend.ModelTrans.gameObject.activeSelf)
                 tdsRend.ModelTrans.gameObject.SetActive(true);
             anim.Play(SharedData.DefaultAnim, 0);
-            anim.Update(1f);
+            anim.Update(1f); //pump the animator
 
-            ImposterPreRenderer.Instance.IssueCommand(skinRend, BillboardTarget, SharedData.Offset, SharedData.Scale, SharedData.Rot);
+            //bake a new mesh using the state of the real one then setup initial rendering data
+            var tempMesh = new Mesh();
+            skinRend.BakeMesh(tempMesh, true);
+            var meshW = Matrix4x4.TRS(SharedData.Offset, Quaternion.Euler(SharedData.Rot), Vector3.one * SharedData.Scale);
+            var cameraVP = Matrix4x4.Ortho(0, 1, 0, 1, 0.1f, 100);
 
-            //restore
+            //render all submeshes
+            BlitMesh(BillboardTarget, tempMesh, skinRend.sharedMaterials, meshW, cameraVP);
+
+            //restore the real mesh to its previous state and cleanup
             anim.Play(info.fullPathHash);
             anim.Update(0);
             anim.cullingMode = oldCull;
             if (tdsRend.ModelTrans.gameObject.activeSelf != oldState)
                 tdsRend.ModelTrans.gameObject.SetActive(oldState);
+            Destroy(tempMesh);
         }
 
+        /// <summary>
+        /// Helper for drawing a one-off mesh to a render texture. This will render all submeshes and materials associated with it.
+        /// </summary>
+        /// <param name="rt"></param>
+        /// <param name="mesh"></param>
+        /// <param name="material"></param>
+        /// <param name="meshW"></param>
+        /// <param name="cameraVP"></param>
+        /// <param name="subMeshIndex"></param>
+        public static void BlitMesh(RenderTexture rt, Mesh mesh, Material[] materials, Matrix4x4 meshW, Matrix4x4 cameraVP)
+        {
+            //sometimes this will not be null and screw us.
+            //special thanks goes to a random github and someone by the name of @guycalledfrank on the internet for figuring this out
+            if (Camera.current != null)
+                cameraVP *= Camera.current.worldToCameraMatrix.inverse;
 
+            RenderTexture oldRt = RenderTexture.active;
+            RenderTexture.active = rt; bool oldCulling = GL.invertCulling;
+            GL.invertCulling = true; //another wonderful contribution from the internet!! no idea why but it works!!
+
+            GL.Clear(true, true, Color.clear);
+            GL.PushMatrix();
+            GL.LoadProjectionMatrix(cameraVP);
+
+            for (int i = 0; i < mesh.subMeshCount; i++)
+            {
+                materials[i].SetPass(0);
+                Graphics.DrawMeshNow(mesh, meshW, i);
+            }
+
+            //now set everything back to the way it was before
+            GL.PopMatrix();
+            GL.invertCulling = oldCulling;
+            RenderTexture.active = oldRt;
+
+        }
+
+        /// <summary>
+        /// Helper for drawing a one-off mesh to a render texture.
+        /// </summary>
+        /// <param name="rt"></param>
+        /// <param name="mesh"></param>
+        /// <param name="material"></param>
+        /// <param name="meshW"></param>
+        /// <param name="cameraVP"></param>
+        /// <param name="subMeshIndex"></param>
+        public static void BlitMesh(RenderTexture rt, Mesh mesh, Material material, Matrix4x4 meshW, Matrix4x4 cameraVP, int subMeshIndex = 0)
+        {
+            //sometimes this will not be null and screw us.
+            //special thanks goes to a random github and someone by the name of @guycalledfrank on the internet for figuring this out
+            if (Camera.current != null)
+                cameraVP *= Camera.current.worldToCameraMatrix.inverse;
+
+            RenderTexture oldRt = RenderTexture.active;
+            RenderTexture.active = rt;
+            bool oldCulling = GL.invertCulling;
+            GL.invertCulling = true; //another wonderful contribution from the internet!! no idea why but it works!!
+
+            GL.Clear(true, true, Color.clear);
+            GL.PushMatrix();
+            GL.LoadProjectionMatrix(cameraVP);
+
+            material.SetPass(0);
+            Graphics.DrawMeshNow(mesh, meshW, subMeshIndex);
+
+            //now set everything back to the way it was before
+            GL.PopMatrix();
+            GL.invertCulling = oldCulling;
+            RenderTexture.active = oldRt;
+
+        }
     }
+
+
 }
